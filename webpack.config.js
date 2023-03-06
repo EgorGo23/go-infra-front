85c48f6de12f9f78e55861afe25d62d580b2f5d3
Egor <egor.golenok@softswiss.com>
2023-03-06 10:22:17 +0300

    add react, ts, prettier, webpack, eslint

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/public/index.html',
+    }),
+  ],
+};
