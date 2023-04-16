17e8a78074fc6f03b87a72b2eab89e8fb6c4f9fa
Egor <egor.golenok@softswiss.com>
2023-04-16 20:42:01 +0300

    add webpack prod config

---
+++ webpack.prod.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, 'src', 'index.tsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src', 'components'),
+      '@types': path.resolve(__dirname, 'src', 'types'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 4552,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'public', 'index.html'),
+    }),
+    new Dotenv(),
+  ],
+};
